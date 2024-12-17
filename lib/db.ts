6aac7300e302c80587077a46f6290eb771e04486
XavierCruz5106 <superguyman121@gmail.com>
2024-12-17 09:51:50 -0500

    feat: turso stuffs

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-const prisma = new PrismaClient();
+const libsql = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.AUTH_TOKEN!,
+})
 
-export default prisma;
+const adapter = new PrismaLibSQL(libsql)
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+  // @ts-ignore
+  adapter,
+})
+
+export default prisma
